97974c13ce63d97d4d97da973579b49480b78d4d
Ayoub Malek <kogito@hotmail.fr>
2019-12-29 14:21:48 +0100

    update and improve tests

--- tests/test_check.py
+++ tests/test_check.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import os
 import pytest
+import configparser
+from core.fileproc import get_file_paths
 from check import clone_repo, del_repo, check_repo
 
 
@@ -23,12 +25,59 @@ def test_clone_and_del_repo(git_path):
                                         ["tests/test_files/sample_test_file.py"],
                                         ["tests/test_files/sample_test_file.rst"]])
 @pytest.mark.parametrize('print_all', [False, True])
-def test_check_repo(file_paths, print_all):
+@pytest.mark.parametrize('white_listed_urls', [["https://github.com/SuperKogito/SuperKogito.github.io"]])
+@pytest.mark.parametrize('white_listed_patterns', [[], ["https://github.com/SuperKogito/SuperKogito.github.io"]])
+def test_check_repo(file_paths,
+                    print_all,
+                    white_listed_urls,
+                    white_listed_patterns):
     """
     test check repo function.
     """
-    check_repo(file_paths, print_all)
+    check_repo(file_paths, print_all, white_listed_urls, white_listed_patterns)
 
-@pytest.mark.xfail
-def test_script():
+
+@pytest.mark.parametrize('config_fname', ['_local_test_config.conf'])
+def test_script(config_fname):
+    # init config parser
+    config = configparser.ConfigParser()
+    config.read(config_fname)
+
+    # init env variables
+    os.environ["INPUT_GIT_PATH"] = config['DEFAULT']["git_path_test_value"]
+    os.environ["INPUT_FILE_TYPES"] = config['DEFAULT']["file_types_test_values"]
+    os.environ["INPUT_PRINT_ALL"] = config['DEFAULT']["print_all_test_value"]
+    os.environ["INPUT_WHITE_LISTED_URLS"] = config['DEFAULT']["white_listed_test_urls"]
+    os.environ["INPUT_WHITE_LISTED_PATTERNS"] =  config['DEFAULT']["white_listed__test_patterns"]
+    # excute script
     os.system("python3 check.py")
+
+
+@pytest.mark.parametrize('local_folder_path', ['./tests/test_files'])
+@pytest.mark.parametrize('config_fname', ['./tests/_local_test_config.conf'])
+def test_locally(local_folder_path, config_fname):
+    # init config parser
+    config = configparser.ConfigParser()
+    config.read(config_fname)
+
+    # read input variables
+    git_path = local_folder_path
+    file_types = config['DEFAULT']["file_types_test_values"].split(",")
+    print_all = config['DEFAULT']["print_all_test_value"]
+    white_listed_urls = config['DEFAULT']["white_listed_test_urls"].split(",")
+    white_listed_patterns = config['DEFAULT']["white_listed__test_patterns"].split(",")
+
+    # debug prints
+    print(" config")
+    print(" -------")
+    print("%25s : %10s" % ("git_path", git_path))
+    print("%25s : %10s" % ("file_types", file_types))
+    print("%25s : %10s" % ("white_listed_urls", white_listed_urls))
+    print("%25s : %10s" % ("white_listed_patterns", white_listed_patterns))
+
+    # get all file paths
+    file_paths = get_file_paths(git_path, file_types)
+
+    # check repo urls
+    check_repo(file_paths, print_all, white_listed_urls, white_listed_patterns)
+    print("Done.")
--- tests/test_files/sample_test_file.md
+++ tests/test_files/sample_test_file.md
@@ -1,6 +1,49 @@
 # This is a test file
-The following is a list of urls to extract.
-[url1](https://www.google.com/)
-[url2](https://github.com/SuperKogito)
-[url3](https://github.com/SuperKogito/URLs-checker/README.md)
-["broken_url"](https://none.html)
+The following is a list of test urls to extract.
+ - [test url 1](https://www.google.com/)
+ - [test url 2](https://github.com/SuperKogito)
+ - [test url 3](https://github.com/SuperKogito/URLs-checker)
+ - [test url 4](https://github.com/SuperKogito/URLs-checker/blob/master/README.md)
+ - [test url 5](https://github.com/SuperKogito/URLs-checker/issues)
+ - [test url 6](https://github.com/SuperKogito/URLs-checker/issues/1)
+ - [test url 7](https://github.com/SuperKogito/URLs-checker/issues/2)
+ - [test url 8](https://github.com/SuperKogito/URLs-checker/issues/3)
+ - [test url 9](https://github.com/SuperKogito/URLs-checker/issues/4)
+
+- [test url 10](https://github.com/SuperKogito/spafe/)
+- [test url 11](https://github.com/SuperKogito/spafe/issues)
+- [test url 12](https://github.com/SuperKogito/spafe/issues/1)
+- [test url 13](https://github.com/SuperKogito/spafe/issues/2)
+- [test url 14](https://github.com/SuperKogito/spafe/issues/3)
+- [test url 15](https://github.com/SuperKogito/spafe/issues/4)
+- [test url 16](https://github.com/SuperKogito/spafe/issues/5)
+- [test url 17](https://github.com/SuperKogito/spafe/issues/6)
+- [test url 18](https://github.com/SuperKogito/spafe/issues/7)
+- [test url 19](https://github.com/SuperKogito/spafe/issues/8)
+
+- [test url 20](https://github.com/SuperKogito/Voice-based-gender-recognition)
+- [test url 21](https://github.com/SuperKogito/Voice-based-gender-recognition/issues)
+- [test url 22](https://github.com/SuperKogito/Voice-based-gender-recognition/issues/1)
+- [test url 23](https://github.com/SuperKogito/Voice-based-gender-recognition/issues/2)
+- [test url 24](https://github.com/SuperKogito/Voice-based-gender-recognition/issues)
+
+- [broken_test url 1](https://none.html)
+- [broken_test url 2](https://github.com/SuperKogito/URLs-checker/README.md)
+
+- [test email 1]("http.sk@info.github.io)
+- [test email 2]("superkogito@github.io)
+- [test email 3]("wwwcom@github.io)
+- [test email 4]("www.com@github.io)
+- [test email 5]("https@github.io)
+- [test email 6]("skogito@github.io)
+- [test email 7]("mailto:superkogito%40gmail.com")
+
+# white-listed URLs
+The following is a list of white-listed urls to ignore.
+ - [white-listed url 1](https://github.com/SuperKogito/URLs-checker/issues/2)
+ - [white-listed url 2](https://github.com/SuperKogito/URLs-checker/issues/3)
+
+# white-listed URLs pattern
+The following is a list of white-listed patterns to ignore.
+ - [white-listed urls pattern 1](https://github.com/SuperKogito/Voice-based-gender-recognition/issues)
+ - [white-listed urls pattern 2](https://img.shields.io/)
